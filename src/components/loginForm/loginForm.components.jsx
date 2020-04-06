35422e7fc3c7489da2c89c8919af5d1e07294267
JhonCowNut <55220275+JohnCowNut@users.noreply.github.com>
2020-04-06 19:01:36 +0700

    Write factories make for server Socket io

--- src/components/loginForm/loginForm.components.jsx
+++ src/components/loginForm/loginForm.components.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./loginForm.styles.scss";
-
+import {VERIFY_USER} from  '../../Event';
 
 class LoginForm extends React.Component{
 	constructor(props) {
@@ -8,15 +8,44 @@ class LoginForm extends React.Component{
 
 		this.state= {
 			nickName: "",
-			errr: ""
+			error: ""
 		}
 	}
+	setUser = ({user,isUser}) => {
+		if(isUser){
+			this.setError("User name taken");
+			alert("User name taken")
+		}else {
+			this.props.setUser(user)
+		}
+	}
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		const {socket} = this.props;
+		const {nickname} = this.state;
+		socket.emit(VERIFY_USER,nickname,this.setUser)
+	}
+	handleChange = (e) => {
+		e.preventDefault()
+		this.setState({nickname: e.target.value});
+		console.log(this.state.nickname)
+	}
+	setError = (error) => {
+		this.setState({error})
+	}
 	render() {
+		const {nickname} =this.state;
 		return (
 			<div className="header" >
-				<form className= "header__form">
+				<form className= "header__form" onSubmit={this.handleSubmit}>
 					<input type="text" className="header__input"
-					 placeholder="Please Type Your Cool Name !"/>
+					 placeholder="Please Type Your Cool Name !"
+					 id="nickname"
+					 ref= {(input) => this.textInput = input}
+					 value={nickname}
+					 onChange = {this.handleChange}
+					 />
 				</form>
 			</div>
 		);
